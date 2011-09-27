60fb386840e78b8e7c2a022f133661bd38a6b269
Heverson Vasconcelos <heverson.vasconcelos@gmail.com>
2011-09-26 23:37:24 -0400

    Adicionada a estrutura Controller e View

---
+++ locacaoautomoveis-web/src/java/br/facom/ufms/locacaoautomoveis/ui/controller/LoginBean.java
@@ -0,0 +1,115 @@
+package br.facom.ufms.locacaoautomoveis.ui.controller;
+
+import br.facom.ufms.locacaoautomoveis.model.dao.UsuarioDAO;
+import br.facom.ufms.locacaoautomoveis.model.daoimpl.UsuarioDAOImpl;
+import br.facom.ufms.locacaoautomoveis.model.entities.Usuario;
+import br.facom.ufms.locacaoautomoveis.ui.util.Constants;
+import br.facom.ufms.locacaoautomoveis.ui.util.FacesUtil;
+import br.facom.ufms.locacaoautomoveis.ui.util.SessionUtil;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * Bean gerenciavel utilizado no controle do login de usuarios na aplicacao.
+ * Este controle envolve principalmente:
+ *                          verificao do login e senha;
+ *                          login e logout;
+ *                          verificar se o usuario esta autenticado e logado;
+ *
+ * @author heverson.vasconcelos
+ */
+@ManagedBean
+@RequestScoped
+public class LoginBean {
+
+    /**
+     * Armazena o usuario corrente
+     */
+    private Usuario usuario;
+    /**
+     * Armazena o usuario corrente apos a autenticacao, ou seja, o usuario
+     * logado na sessao.
+     */
+    private Usuario usuarioAutenticado;
+    /**
+     * Variavel utilizada nos metodos que irao inserir ou consultar alguma
+     * informacao relativa aos usuarios.
+     */
+    private UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
+
+    @PostConstruct
+    public void init() {
+        usuario = new Usuario();
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario userToauthenticate) {
+        this.usuario = userToauthenticate;
+    }
+
+    /**
+     * Metodo para verificar o login do usuario.
+     * Consulta um determinado a partir do nome de login.
+     *
+     * @return Usuario encontrado. Null caso o usuario nao seja encontrado.
+     */
+    private Usuario verificarLogin(Usuario usuario) {
+
+        return (usuarioDAO.buscarUsuarioPeloNomeLogin(usuario.getNomeLogin()));
+    }
+
+    /**
+     * Metodo para verificar a senha do usuario.
+     *
+     * @return True caso a senha esteja correta.
+     */
+    private boolean verificarSenha() {
+        if ((usuarioAutenticado.getSenha().compareTo(usuario.getSenha()) == 0)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Metodo para autenticar o usuario. Verifica o login e a senha deste.
+     *
+     * @return String contendo o endereco de redirecionamento caso o usuario foi
+     *          corretamente autenticado. <br>
+     *          Null caso houver um erro na autenticacao.
+     */
+    public String login() {
+        usuarioAutenticado = verificarLogin(usuario);
+
+        if (usuarioAutenticado == null) {
+            FacesUtil.mensErro(Constants.MSG_INVALID_USER);
+            return null;
+        } else if (!verificarSenha()) {
+            usuarioAutenticado = null;
+            FacesUtil.mensErro(Constants.MSG_INVALID_PASSWORD);
+            return null;
+        }
+
+        SessionUtil.setAttribute(Constants.LOGGED_USER, usuarioAutenticado);
+
+        return Constants.PAGE_LOCACOES;
+    }
+
+    /**
+     * Metodo para executar o logout da sessao.
+     *
+     * @return String contendo o endereco de redirecionamento para a pagina
+     *          inicial (index.xhtml).
+     */
+    public String logout() {
+        usuario = new Usuario();
+        usuarioAutenticado = null;
+        SessionUtil.destroySession();
+
+        return Constants.PAGE_INDEX;
+    }
+}
---
+++ locacaoautomoveis-web/src/java/br/facom/ufms/locacaoautomoveis/ui/listener/LoggedInCheck.java
@@ -0,0 +1,43 @@
+package br.facom.ufms.locacaoautomoveis.ui.listener;
+
+import br.facom.ufms.locacaoautomoveis.ui.util.Constants;
+import br.facom.ufms.locacaoautomoveis.ui.util.SessionUtil;
+import javax.faces.application.NavigationHandler;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+/**
+ *
+ * @author heverson.vasconcelos
+ */
+public class LoggedInCheck implements PhaseListener {
+
+    public static final String PAGE_INDEX_FILENAME = Constants.PAGE_INDEX + ".xhtml";
+    public static final String PAGE_USER_NOT_LOGGED_FILENAME = Constants.PAGE_USER_NOT_LOGGED + ".xhtml";
+
+    @Override
+    public void afterPhase(PhaseEvent event) {
+        FacesContext facesContext = event.getFacesContext();
+        String currentPage = facesContext.getViewRoot().getViewId();
+
+        boolean isLoginPage = (currentPage.lastIndexOf(PAGE_INDEX_FILENAME) > -1);
+        boolean isUserNotLogged = (currentPage.lastIndexOf(PAGE_USER_NOT_LOGGED_FILENAME) > -1);
+        Object currentUser = SessionUtil.getAttribute(Constants.LOGGED_USER);
+
+        if (!isLoginPage && !isUserNotLogged && currentUser == null) {
+            NavigationHandler nh = facesContext.getApplication().getNavigationHandler();
+            nh.handleNavigation(facesContext, null, Constants.PAGE_USER_NOT_LOGGED);
+        }
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent event) {
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.RESTORE_VIEW;
+    }
+}
