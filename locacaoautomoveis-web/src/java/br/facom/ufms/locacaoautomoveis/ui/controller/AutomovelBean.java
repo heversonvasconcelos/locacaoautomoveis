cc5ddc0f9d1cefeb9a1c1cb73f0d97b5528fa113
Agripino Duarte <agripinoduarte@gmail.com>
2011-10-09 01:28:36 -0400

    implementando a view de automoveis. tentando imprimir a lista( nada imprime)

--- locacaoautomoveis-web/src/java/br/facom/ufms/locacaoautomoveis/ui/controller/AutomovelBean.java
+++ locacaoautomoveis-web/src/java/br/facom/ufms/locacaoautomoveis/ui/controller/AutomovelBean.java
@@ -5,19 +5,36 @@ package br.facom.ufms.locacaoautomoveis.ui.controller;
 
 import br.facom.ufms.locacaoautomoveis.model.dao.AutomovelDAO;
 import br.facom.ufms.locacaoautomoveis.model.daoimpl.AutomovelDAOImpl;
+import br.facom.ufms.locacaoautomoveis.model.entities.Automovel;
+import java.io.Serializable;
+import java.util.List;
 import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
 
-@ManagedBean
-@ViewScoped
+
 /**
  * Interface principal para gerenciar automóveis
  */
-
-public class AutomovelBean {
+@ManagedBean
+@ViewScoped
+public class AutomovelBean implements Serializable{
     
-    private AutomovelDAO automovel = new AutomovelDAOImpl();
+    private AutomovelDAO automovelDao;
+    private Automovel automovel;
     
+    @PostConstruct
+    public void init() {
+        automovelDao = new AutomovelDAOImpl();
+        automovel = new Automovel();
+    }
     
+    public Automovel getAutomovel() {
+        return automovel;
+    }
+    
+    public List<Automovel> getListaAutomoveis() {
+        return automovelDao.list();
+    } 
     
 }
