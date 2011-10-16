a920564ea845c8673f68f0916e7bbc57910706fc
Heverson Vasconcelos <heverson.vasconcelos@gmail.com>
2011-10-15 21:53:52 -0400

    Removido jQuery e javascript; Adicionado menu do primefaces

--- locacaoautomoveis-web/web/resources/js/datepicker.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(function() {
-	$(".term").datepicker({
-	    dateFormat: 'dd/mm/yy',
-	    dayNamesMin: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
-	    monthNames: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-	});
-    });
-});
